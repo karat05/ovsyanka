8ec6d5bf0c0cbc55df4a8ff802548892119235ab
karat05 <yaric23x@gmail.com>
2026-03-25 10:29:22 +0300

    Update README files - make main README concise, move detailed docs to skriptRepo

---
+++ myNotes/push_v2.ps1
@@ -0,0 +1,4 @@
+Set-Location "c:\Users\user\ovsyanka\myNotes"
+git add .
+git commit --message "Update README files - make main README concise, move detailed docs to skriptRepo"
+git push origin master
